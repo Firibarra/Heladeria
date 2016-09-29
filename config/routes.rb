9573ca2180755222555c38dc4ca9268a0cb8b6c4
Francisco <Francisco.iri_95@hotmail.es>
2016-09-29 12:22:26 -0300

    Agrege la gema de activeadmin falta su configuracion

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :vendedors
   #root to: "windows#entrada"
   root to: "clientes#index"
